da3494c416ce492e0fcb3d61a6c7c928ca3330f5
Dmitry Glyuz <deceivedxchild@gmail.com>
2020-10-15 05:50:26 +0300

    edit main.py

--- main.py
+++ main.py
@@ -1,235 +1,257 @@
 import random
 
-# Initializing field
-field_dict = {}                                     # Dictionary with field's cells
-abc = ['A', 'B', 'C']                               # List with columns
-numbers = [str(number) for number in range(1, 4)]   # List with strings
-
-# Fill the dictionary
-for letter in abc:
-    for number in numbers:
-        field_dict.update({letter + number: '-'})
-
-# List with available cells
-available_cells = list(field_dict.keys())
-
-# Making scan list
-scan_list = []
-# Horizontal lines
-for number in numbers:
-    scan_list.append([letter + number for letter in abc])
-# Vertical lines
-for letter in abc:
-    scan_list.append([letter + number for number in numbers])
+# Constants with columns
+ABC = ['A', 'B', 'C']
+# And strings
+NUMBERS = [str(number) for number in range(1, 4)]
 
 
-# Diagonal lines
-def add_diagonal(numbers_list):
-    scan_list.append([cell_letter + cell_number for cell_letter, cell_number in zip(abc, numbers_list)])
+class Board:
+    class Cell:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+            self.value = '-'
+            self.marked = False
+            self.on_lines = []
+
+        def __str__(self):
+            return self.value
+
+        def mark(self, sign):
+            if sign in ('X', 'O'):
+                self.value = sign
+                self.marked = True
+                for line in self.on_lines:
+                    line.update()
+            else:
+                raise ValueError
+
+    class Line():
+        def __init__(self, c1, c2, c3):
+            self.c1 = c1
+            self.c2 = c2
+            self.c3 = c3
+            self.free = True
+            self.cells = (self.c1, self.c2, self.c3)
+            self.ids = ''
+            for cell in self.cells:
+                self.ids += f'{cell.x}{cell.y} '
+                cell.on_lines.append(self)
+            self.crossed_out = False
+            self.crossed_by = None
+            self.full = False
+            self.counter = {
+                'X': 0,
+                'O': 0
+            }
+
+        def show(self):
+            return f'{self.c1} {self.c2} {self.c3}'
+
+        def update(self):
+            for key in self.counter.keys():
+                self.counter[key] = 0
+            for cell in self.cells:
+                if cell.marked is True:
+                    self.counter[cell.value] += 1
+                    self.free = False
+            for sign, number in self.counter.items():
+                if number == 3:
+                    self.crossed_out = True
+                    self.crossed_by = sign
+            if sum(self.counter.values()) == 3:
+                self.full = True
+
+    def __init__(self):
+        # Create cells
+        self.cells = {}
+        for x in ABC:
+            for y in NUMBERS:
+                self.cells.update({x + y: self.Cell(x, y)})
+
+        # Create lines
+        def diagonal(nums):
+            return self.Line(*[self.cells[x + y] for x, y in zip(ABC, nums)])
+        self.lines = {
+            'horizontal': [],
+            'vertical': [],
+            'diagonal': [diagonal(NUMBERS), diagonal(reversed(NUMBERS))]
+        }
+        for y in NUMBERS:
+            self.lines['horizontal'].append(self.Line(*[self.cells[x + y] for x in ABC]))
+        for x in ABC:
+            self.lines['vertical'].append(self.Line(*[self.cells[x + y] for y in NUMBERS]))
+
+    def __str__(self):
+        output = '\n  A B C'
+        for n in range(3):
+            output += f"\n{n + 1} {self.lines['horizontal'][n].show()}"
+        return output
 
 
-add_diagonal(numbers)
-add_diagonal(reversed(numbers))
+class Player:
+    signs = ('X', "O")
+    available_signs = list(signs)
+    log = []
+
+    def __init__(self, sign=None, area=None):
+        self.board = area
+
+        if sign:
+            if sign in self.signs:
+                self.sign = sign
+                self.available_signs.remove(sign)
+            else:
+                raise ValueError
+
+        else:
+            self.sign = self.available_signs.pop()
+        self.enemy_sign = self.signs[1] if self.sign == self.signs[0] else self.signs[0]
+
+    def turn(self, key):
+        self.board.cells[key].mark(self.sign)
+        self.log.append((self.sign, key))
+
+    def check_available_cells(self):
+        available_cells = []
+        for id, cell in self.board.cells.items():
+            if not cell.marked:
+                available_cells.append(id)
+        return available_cells
+
+    def random_decision(self):
+        self.turn(random.choice(self.check_available_cells()))
+
+    def smart_decision(self):
+        available_cells = self.check_available_cells()
+        # print(f'Доступные ячейки:\n {available_cells}\nПроверяю каждую.')
+        # Dictionary with priorities
+        priorities = {
+            1: [],  # leads to victory
+            2: [],  # prevents opponent's victory
+        }
+        rating = {}
+        for cell_id in available_cells:
+            rating[cell_id] = 0
+
+        for cell_id in available_cells:
+            free_lines = 0
+            cell = self.board.cells[cell_id]
+            # print(f'\nЯчейка {cell_id}:\nНаходится на линиях {[line.ids for line in board.cells[cell_id].on_lines]}')
+            for line in cell.on_lines:
+                if line.counter[self.sign] == 2:
+                    priorities[1].append(cell_id)
+                    break
+                elif line.counter[self.enemy_sign] == 2:
+                    priorities[2].append(cell_id)
+                    break
+                else:
+                    if line.free:
+                        rating[cell_id] += 1
+                        free_lines += 1
+                    if not line.full:
+                        rating[cell_id] += 1
+                        if line.counter[self.sign] == 1:
+                            rating[cell_id] += 1
+            if len(cell.on_lines) == 3 and free_lines == 3:
+                rating[cell_id] += 3
+
+        if priorities[1]:
+            self.turn(random.choice(priorities[1]))
+        elif priorities[2]:
+            self.turn(random.choice(priorities[2]))
+        else:
+            max_rating = max(rating.values())
+            max_rating_cells = []
+            for k, v in rating.items():
+                if v == max_rating:
+                    max_rating_cells.append(k)
+            self.turn(random.choice(max_rating_cells))
 
 
-# This function returns a list of values from cells contained in the list specified as a parameter
-def get_content(line_in_list):
-    return [field_dict[key] for key in line_in_list]
+class Game:
+    def __init__(self, brd):
+        self.board = brd
+        self.winning_sign = None
+        pass
 
+    def is_over(self):
+        for cell in self.board.cells.values():
+            if cell.marked is False:
+                break
+        else:
+            return True
 
-# This function shows field
-def show_field():
-    print('\n  A B C')
-    for number in numbers:
-        # Numerate the line
-        field_line = number
-
-        # Fill the line
-        for show_field_letter in abc:
-            field_line += ' ' + field_dict[show_field_letter + number]
-        print(field_line)
-
-
-# Update field
-def update_field(cell_name, sign):
-    # Open access to write game_over Boolean
-    global game_over
-
-    # This function checks if there is a sign X or O on the line that repeats three times and returns it if so
-    def check_for_winner(line_to_check):
-        for xo_sign in ['X', 'O']:
-            if line_to_check.count(xo_sign) == 3:
-                return xo_sign
+        for category in self.board.lines.values():
+            for line in category:
+                if line.crossed_out:
+                    self.winning_sign = line.crossed_by
+                    return True
         else:
             return False
 
-    # Change the value in specified cell
-    field_dict[cell_name] = sign
-
-    # Remove this cell from available cells list
-    available_cells.remove(cell_name)
-
-    # Show updated field
-    show_field()
-
-    # Scan lines
-    # Go through scan_list
-    for line in scan_list:
-        # Get signs from current line
-        line_content = get_content(line)
-
-        # Check if there are three signs
-        win_sign = check_for_winner(line_content)
-
-        # Stop scanning if someone wins
-        if win_sign is not False:
-            break
-
-    # Show message if someone wins
-    if win_sign is not False:
-        final = 'You won!' if user_sign == win_sign else 'You lost!'
-        print(final)
-        game_over = True
-
-    # Show message if there are no available cells
-    elif not available_cells:
-        print('No available cells.')
-        game_over = True
+    def info(self):
+        print('Horizontal:')
+        for (k, v) in board.lines.items():
+            print(f'{k.title()}:')
+            for line in v:
+                print(f'{line.ids} - {"free" if line.free else "not free"}')
 
 
-# Computer's turn
-def computer_turn():
-    # Dictionary with priorities
-    priorities = {
-        1: [],      # leads to victory
-        2: [],      # prevents opponent's victory
-        3: {}       # other cells, with rating
-    }
-
-    available_lines = {}
-
-    # This function puts a third sign in the line to defeat or prevent the opponent from winning
-    def add_third_cell(priority):
-        # If priority is 1, it checks computer's signs
-        # If 2 it checks user's signs
-        sign = pc_sign if priority == 1 else user_sign
-        if line_content.count(sign) == 2 and '-' in line_content:
-            priorities[priority].append(line[line_content.index('-')])
-
-    for line in scan_list:
-        # Fill the line to scan
-        line_content = get_content(line)
-
-        # Add cells which lead to victory to list with the first priority, don't scan nothing else
-        if priorities[1] or (line_content.count(pc_sign) == 2 and '-' in line_content):
-            add_third_cell(1)
-        # Add cells which prevent opponent's victory
-        elif line_content.count(user_sign) == 2 and '-' in line_content:
-            add_third_cell(2)
-    # If this list is not empty
-    if priorities[1]:
-        # Take random cell from priorities[1]
-        pc_turn = random.choice(priorities[1])
-    # Else if this list is not empty
-    elif priorities[2]:
-        # Take random cell from priorities[2]
-        pc_turn = random.choice(priorities[2])
-    else:
-        for cell in field_dict.keys():
-            # Make a key in priorities[3] dictionary
-            priorities[3][cell] = 0
-            available_lines[cell] = []
-
-            # Make a list with lines which current cell is included to
-            for line in scan_list:
-                if cell in line:
-                    available_lines[cell].append(line)
-
-            # Count priority
-            # Check all lines with current cell
-            for line in available_lines[cell]:
-                line_content = get_content(line)
-                # +1 if there is a line with one user's sign and nothing else
-                if user_sign in line_content and pc_sign not in line_content:
-                    priorities[3][cell] += 1
-                # +1 If there is a line with one computer's sign and nothing else
-                if pc_sign in line_content and user_sign not in line_content:
-                    priorities[3][cell] += 1
-                # +1 if there is a free line
-                if line_content.count('-') == 3:
-                    priorities[3][cell] += 1
-
-        # Find max priority
-        max_priority = 0
-        for v in priorities[3].values():
-            if v > max_priority:
-                max_priority = v
-        # Make a list with cells with max priority
-        max_priority_cells = []
-        for k, v in priorities[3].items():
-            if v == max_priority:
-                if k in available_cells:
-                    max_priority_cells.append(k)
-
-        # Take a random value from the list with cells with max priority
-        if max_priority_cells:
-            pc_turn = random.choice(max_priority_cells)
-        # Or take any random value
-        else:
-            pc_turn = random.choice(available_cells)
-    print(f'\nMy turn: {pc_turn}')
-    update_field(pc_turn, pc_sign)
-
-
+#         # Add cells which lead to victory to list with the first priority, don't scan nothing else
+#         # Add cells which prevent opponent's victory
+#         # Take random cell from priorities[1]
+#     # Else if this list is not empty
+#         # Take random cell from priorities[2]
 if __name__ == '__main__':
+
     # Greeting
     print("Hello! Let's play TIC-TAC-TOE!\n"
           "Choose who will you play for:\n"
-          "   1 - cross (X)\n"
-          "   2 - zeros (O)")
+          "   X - cross\n"
+          "   O - zeros\n"
+          "   Z - Computer vs. Computer")
+    board = Board()
 
-    # Select who turns first
-    choice = None
-    user_sign = ''
-    while user_sign == '':
-        choice = input('Your choice: ')
-        if choice == '1':
-            user_sign, pc_sign = 'X', 'O'
-        elif choice == '2':
-            user_sign, pc_sign = 'O', 'X'
-        else:
-            print('Wrong value. Try again.')
-
-    show_field()
+    game = Game(board)
+    choice = ''
+    while choice not in Player.signs and choice != 'Z':
+        choice = input('Your choice: ').upper()
+    user = Player(sign=choice if choice in Player.signs else None, area=board)
+    pc = Player(area=board)
 
     # Show message who turns first
-    # and start the game
-    first_turn = ' make the first turn!'
-    if pc_sign == 'X':
-        print('I' + first_turn)
-        user_turn = False
-    else:
-        print('You' + first_turn)
-        user_turn = True
-    game_over = False
-    while game_over is False:
+    print(f"{'You' if user.sign == 'X' else 'I'} make the first turn!")
+    print(board)
+    # Start the game
+    while not game.is_over():
         # User's turn
-        if user_turn is True:
-            turn_done = False
-            while turn_done is False:
-                turn = input('\nYour turn: ').upper()
-                # Check if input is correct
-                if turn in available_cells:
-                    update_field(turn, user_sign)
-                    turn_done = True
-                elif turn in field_dict.keys():
-                    print('This cell is already filled. Try again.')
-                else:
-                    print('Wrong value. Try again!')
-            user_turn = False
-        # Computer's turn
-        else:
-            computer_turn()
-            user_turn = True
 
+        if not Player.log and user.sign == 'X' or Player.log and Player.log[-1][0] == pc.sign:
+            turns = len(Player.log)
+            while not len(Player.log) > turns:
+                if choice == 'Z':
+                    user.smart_decision()
+                    print(f"{user.sign}'s turn: {Player.log[-1][1]}")
+                else:
+                    selected_cell = input('\nYour turn: ').upper()
+                    # Check if input is correct
+                    if selected_cell not in board.cells.keys():
+                        print('Wrong value. Try again!')
+                    elif not board.cells[selected_cell].marked:
+                        user.turn(selected_cell)
+                    else:
+                        print('This cell is already filled. Try again.')
+        else:
+            pc.smart_decision()
+            print(f"\n{'PC' if choice != 'Z' else pc.sign}'s turn: {Player.log[-1][1]} ")
+        print(board)
+    if not game.winning_sign:
+        print('No available cells!')
+    else:
+        if choice == 'Z':
+            print(f'{game.winning_sign} won!')
+        else:
+            print(f'You {"won" if user.sign == game.winning_sign else "lost"}!')
     print('Game over!')
